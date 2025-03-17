3ccffb6e48b39f5c8037145cd3485917225ba389
Manheten22 <138751238+Manheten22@users.noreply.github.com>
2025-03-17 15:19:58 -0700

    RS.lua

--- RS.lua
+++ RS.lua
@@ -43,8 +43,8 @@ type Tab = {
 -- Variables
 
 local ApplyUnsupportedName: (Name: string, Condition: boolean) -> (string) = getgenv().ApplyUnsupportedName
---local HandleConnection: (Connection: RBXScriptConnection, Name: string) -> () = getgenv().HandleConnection
-local Notify: (Title: string, Content: string, Image: string?) -> () = getgenv().Notify
+local HandleConnection: (Connection: RBXScriptConnection, Name: string) -> () = getgenv().HandleConnection
+--local Notify: (Title: string, Content: string, Image: string?) -> () = getgenv().Notify
 local GetClosestChild: (Children: {PVInstance}, Callback: ((Child: PVInstance) -> () | boolean)?, MaxDistance: number?) -> PVInstance? = getgenv().GetClosestChild
 local CreateFeature: (Tab: Tab, FeatureName: string) -> () = getgenv().CreateFeature
 
