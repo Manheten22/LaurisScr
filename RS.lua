5ff82486b16ba8f8cb10b44a5a32c0b4e3c61f90
Manheten22 <138751238+Manheten22@users.noreply.github.com>
2025-03-17 14:42:02 -0700

    RS.lua

--- RS.lua
+++ RS.lua
@@ -4,7 +4,7 @@ local getgenv: () -> ({[string]: any}) = getfenv().getgenv
 getgenv().ScriptVersion = "v0.0.1"
 
 getgenv().Changelog = [[
-	no variables
+
 ]]
 
 do
@@ -42,6 +42,114 @@ type Tab = {
 
 -- Variables
 
+--local ApplyUnsupportedName: (Name: string, Condition: boolean) -> (string) = getgenv().ApplyUnsupportedName
+local HandleConnection: (Connection: RBXScriptConnection, Name: string) -> () = getgenv().HandleConnection
+local Notify: (Title: string, Content: string, Image: string?) -> () = getgenv().Notify
+local GetClosestChild: (Children: {PVInstance}, Callback: ((Child: PVInstance) -> () | boolean)?, MaxDistance: number?) -> PVInstance? = getgenv().GetClosestChild
+local CreateFeature: (Tab: Tab, FeatureName: string) -> () = getgenv().CreateFeature
+
+local Success, Network = pcall(require, game:GetService("ReplicatedStorage").Modules.Network)
+
+local TweenService = game:GetService("TweenService")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+local Flags: Flags = getgenv().Flags
+
+local Player = game:GetService("Players").LocalPlayer
+
+local function GetChildInCharacter(ChildName: string): (RemoteEvent & BasePart & Humanoid)?
+	local Character = Player.Character
+
+	if not Character then
+		return
+	end
+
+	local Child = Character:FindFirstChild(ChildName, true)
+
+	return Child
+end
+
+local LastFired = 0
+
+local function TeleportLocalCharacter(NewLocation: CFrame)
+	local Character = Player.Character
+
+	if not Character then
+		return
+	end
+	
+	local InvisibleParts: Folder = workspace:FindFirstChild("InvisibleParts")
+	
+	if not InvisibleParts then
+		return
+	end
+	
+	local MandrakeRope = InvisibleParts:FindFirstChild("MandrakeRope")
+	
+	if not MandrakeRope then
+		return
+	end
+	
+	local MandrakePit = InvisibleParts:FindFirstChild("MandrakePit") :: Part
+
+	if not MandrakePit then
+		return
+	end
+	
+	if (Character:GetPivot().Position - NewLocation.Position).Magnitude > 50 then
+		if tick() - LastFired >= 2 then
+			local Interact = GetChildInCharacter("Interact")
+
+			if not Interact then
+				return
+			end
+
+			Interact:FireServer({
+				player = Player,
+				Object = MandrakeRope,
+				Action = "Enter"
+			})
+			LastFired = tick()
+		end
+
+		local Start = tick()
+
+		repeat
+			task.wait()
+		until (Character:GetPivot().Position - MandrakePit.Position).Magnitude <= 10 or tick() - Start >= 1
+
+		task.wait(0.1)
+	end
+	
+	Character:PivotTo(NewLocation)
+end
+
+local function EmulateClick()
+	if not Success then
+		return
+	end
+	
+	Network.connect("MouseInput", "Fire", Player.Character, {
+		Config = "Button1Down"
+	})
+	
+	Network.connect("MouseInput", "Fire", Player.Character, {
+		Config = "Button1Up"
+	})
+end
+
+local function IsInvalidMob(Child: PVInstance): ()
+	if Child == Player.Character then
+		return true
+	end
+
+	local Master = Child:FindFirstChild("Master") :: ObjectValue
+
+	if Master and Master.Value == Player.Character then
+		return true
+	end
+end
 
 -- Features
 
