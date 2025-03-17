f2c76d17247faad6e8d6e124465cb64ab424f77a
Manheten22 <138751238+Manheten22@users.noreply.github.com>
2025-03-17 15:18:40 -0700

    RS.lua

--- RS.lua
+++ RS.lua
@@ -4,7 +4,7 @@ local getgenv: () -> ({[string]: any}) = getfenv().getgenv
 getgenv().ScriptVersion = "v0.0.1"
 
 getgenv().Changelog = [[
-	HandleConnection
+	CreateFeature, Sucess
 ]]
 
 do
@@ -43,12 +43,12 @@ type Tab = {
 -- Variables
 
 local ApplyUnsupportedName: (Name: string, Condition: boolean) -> (string) = getgenv().ApplyUnsupportedName
-local HandleConnection: (Connection: RBXScriptConnection, Name: string) -> () = getgenv().HandleConnection
+--local HandleConnection: (Connection: RBXScriptConnection, Name: string) -> () = getgenv().HandleConnection
 local Notify: (Title: string, Content: string, Image: string?) -> () = getgenv().Notify
 local GetClosestChild: (Children: {PVInstance}, Callback: ((Child: PVInstance) -> () | boolean)?, MaxDistance: number?) -> PVInstance? = getgenv().GetClosestChild
---local CreateFeature: (Tab: Tab, FeatureName: string) -> () = getgenv().CreateFeature
+local CreateFeature: (Tab: Tab, FeatureName: string) -> () = getgenv().CreateFeature
 
---local Success, Network = pcall(require, game:GetService("ReplicatedStorage").Modules.Network)
+local Success, Network = pcall(require, game:GetService("ReplicatedStorage").Modules.Network)
 
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
